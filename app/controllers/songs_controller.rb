cad317610f982741b8263f884ccb974f3703f3cc
jerkzilla <johnjamesmarshalljr@gmail.com>
2020-05-21 18:34:55 -0500

    Done.

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,100 @@
+require 'rack-flash'
+
+class SongsController < ApplicationController
+  enable :sessions
+  set :session_secret, "my_application_secret"
+
+  use Rack::Flash
+
+  get '/songs' do
+    @songs = Song.all
+
+    erb :'songs/index'
+  end
+
+  post '/songs' do
+    song = Song.new(name: params[:song][:name])
+    if !params[:song][:artist].nil?
+      song.artist = Artist.find(params[:song][:artist].to_i)
+    elsif !(params[:new_artist_name] == '')
+      artist = Artist.find_by(name: params[:new_artist_name])
+      if artist.nil?
+        artist = Artist.create(name: params[:new_artist_name])
+      end
+      song.artist = artist
+    end
+
+    if !params[:song][:genres].empty?
+      for genre_id in params[:song][:genres]
+        song.genres << Genre.find(genre_id)
+      end
+    end
+    if !(params[:new_genre_name] == '')
+      new_genre = Genre.find_by(name: params[:new_genre_name])
+      if new_genre.nil?
+        new_genre = Genre.create(name: params[:new_genre_name])
+      end
+      song.genres << new_genre
+    end
+    song.save
+
+    flash[:message] = 'Successfully created song.'
+
+    redirect to "/songs/#{song.slug}"
+  end
+
+  get '/songs/new' do
+    @artists = Artist.all
+    @genres = Genre.all
+
+    erb :'songs/new'
+  end
+
+  get '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+
+    erb :'songs/show'
+  end
+
+  patch '/songs/:slug' do
+    song = Song.find_by_slug(params[:slug])
+    if !params[:song][:artist].nil?
+      song.artist = Artist.find_by_id(params[:song][:artist].to_i)
+    end
+    if !(params[:new_artist_name] == '')
+      artist = Artist.find_by(name: params[:new_artist_name])
+      if artist.nil?
+        artist = Artist.create(name: params[:new_artist_name])
+      end
+      song.artist = artist
+    end
+
+    song.genres = []
+    if !params[:song][:genres].empty?
+      for genre_id in params[:song][:genres]
+        song.genres << Genre.find_by_id(genre_id)
+      end
+    end
+    if !(params[:new_genre_name] == '')
+      new_genre = Genre.find_by(name: params[:new_genre_name])
+      if !new_genre.nil?
+        new_genre = Genre.create(name: params[:new_genre_name])
+      end
+      song.genres << new_genre
+    end
+    song.save
+
+    flash[:message] = 'Successfully updated song.'
+
+    redirect to "/songs/#{song.slug}"
+  end
+
+  get '/songs/:slug/edit' do
+    @song = Song.find_by_slug(params[:slug])
+    @artists = Artist.all
+    @genres = Genre.all
+
+    erb :'songs/edit'
+  end
+
+end
---
+++ app/models/artist.rb
@@ -0,0 +1,16 @@
+class Artist < ActiveRecord::Base
+  has_many :songs
+  has_many :song_genres, through: :songs
+  has_many :genres, through: :song_genres
+
+
+  def self.find_by_slug(slug)
+    self.all.select {|artist| artist.slug == slug}.first
+  end
+
+
+  def slug
+    Slugifiable.slugify(self.name)
+  end
+
+end
